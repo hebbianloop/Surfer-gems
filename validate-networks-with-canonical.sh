964fd48c2680b7b8ee988e85cd9f94668cdc7aa5
seldamat <seldamat@gmail.com>
2016-11-11 18:53:56 -0500

    -updated

--- validate-networks-with-canonical.sh
+++ validate-networks-with-canonical.sh
@@ -1,65 +1,43 @@
 #/bin/bash
 
 # Compute cross correlation between ICA components & various canonical templates
+projdir="/Volumes/ADS.Archive.1/ADS/analysis/conn.rest/xcorr/20ICA-validation"
+maskf="/Applications/MATLAB_R2015b.app/toolbox/spm12/toolbox/FieldMap/brainmask.nii"
 
-projdir="/Volumes/Journal.Research.1/Projects/ADS/analysis/conn.rest/xcorr/20ICA-validation"
+# test ICA data
+data=$(ls $projdir/ica/*.nii)
 
-maskf="/Applications/MATLAB_R2015b.app/toolbox/spm12"
-
-test=$(ls $projdir/ica/*.nii)
+# Canonical templates
 
 # 1. Brainmap ICA components
 images=$(ls $projdir/canonical/brainmap20/*.nii)
-savedir="$projdir/calc/ica20.v.brainmap20"
+savedir="$projdir/brainmap20.v.ica20"
+# cross correlate datasets
+dsxcorr -x "$images" -y "$data" -m 'pearson' -s "$savedir" -k "$maskf"
+dsxcorr -x "$images" -y "$data" -m 'dice' -s "$savedir" -k "$maskf"
+dsxcorr -x "$images" -y "$data" -m 'jaccard' -s "$savedir" -k "$maskf"
 
-if [ ! -d $savedir ]; then
-	mkdir -p $savedir
-fi
-
-for i in $images; do
-	for t in $test; do
-		mricalc_xcorr -x $t -y $i -m $maskf -s $savedir -p -d -j
-	done
-done
 
 # 2. Smith Resting State ICA Networks (matches with Brainmap ICA components)
-images=$(ls $projdir/canonical/smith20/*.nii)
-savedir="$projdir/calc/ica20.v.smith20"
-
-if [ ! -d $savedir ]; then
-	mkdir -p $savedir
-fi
-
-for i in $images; do
-	for t in $test; do
-		mricalc_xcorr -x $t -y $i -m $maskf -s $savedir -p -d -j
-	done
-done
+#images=$(ls $projdir/canonical/smith20/*.nii)
+#savedir="$projdir/ica20.v.smith20"
+# cross correlate datasets
+#dsxcorr -x "$images" -y "$data" -m 'pearson' -s "$savedir" -k "$maskf"
+#dsxcorr -x "$images" -y "$data" -m 'jaccard' -s "$savedir" -k "$maskf"
+#dsxcorr -x "$images" -y "$data" -m 'dice' -s "$savedir" -k "$maskf"
 
 # 3. FIND Networks
-images=$(ls $projdir/canonical/find/*.nii)
-savedir="$projdir/calc/ica20.v.find"
-
-if [ ! -d $savedir ]; then
-	mkdir -p $savedir
-fi
-
-for i in $images; do
-	for t in $test; do
-		mricalc_xcorr -x $t -y $i -m $maskf -s $savedir -p -d -j
-	done
-done
+#images=$(ls $projdir/canonical/'find'/*.nii)
+#savedir="$projdir/ica20.v.find"
+# cross correlate datasets
+#dsxcorr -x "$images" -y "$data" -m 'pearson' -s "$savedir" -k "$maskf"
+#dsxcorr -x "$images" -y "$data" -m 'jaccard' -s "$savedir" -k "$maskf"
+#dsxcorr -x "$images" -y "$data" -m 'dice' -s "$savedir" -k "$maskf"
 
 # 4. Kanwisher
-images=$(ls $projdir/canonical/kanwisher-face/*.nii)
-savedir="$projdir/calc/ica20.v.kanwisher-face"
-
-if [ ! -d $savedir ]; then
-	mkdir -p $savedir
-fi
-
-for i in $images; do
-	for t in $test; do
-		mricalc_xcorr -x $t -y $i -m $maskf -s $savedir -p -d -j
-	done
-done
+#images=$(ls $projdir/canonical/kanwisher-face/*.nii)
+#savedir="$projdir/ica20.v.kanwisher-face"
+# cross correlate datasets
+#dsxcorr -x "$images" -y "$data" -m 'pearson' -s "$savedir" -k "$maskf"
+#dsxcorr -x "$images" -y "$data" -m 'jaccard' -s "$savedir" -k "$maskf"
+#dsxcorr -x "$images" -y "$data" -m 'dice' -s "$savedir" -k "$maskf"
